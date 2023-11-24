4dcf1786db6aee8f46a22863114d07b99233b381
Saro <babikiansaro@gmail.com>
2023-11-24 15:49:13 -0800

    styling the active link in the navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import NavBar from "./NavBar";
 export default function Home() {
   return (
     <>
-      <NavBar />
-      <div style={{ padding: 20 }}>Hello World</div>
+      <div>Hello World</div>
     </>
   );
 }
